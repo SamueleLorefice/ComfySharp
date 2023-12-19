45392a3a8aeab3966b561c914fa3f67298a8e055
Samuele Lorefice <samuelelorefice98@gmail.com>
2023-12-19 01:54:19 +0100

    Added JSONserialization options (because this is not a default in 2023, for some reasons, well)

--- ComfySharp/ConversionSettings.cs
+++ ComfySharp/ConversionSettings.cs
@@ -8,7 +8,7 @@ namespace ComfySharp;
 public class ConversionSettings {
     [DataMember(IsRequired = true)] public List<string> EnumConvertAsString = new();
     [DataMember(IsRequired = true)] public List<string> EnumConvertAsBool = new();
-
+    static JsonSerializerOptions jsonOpt = new() { WriteIndented = true, IncludeFields = true };
     public static ConversionSettings FromFile(string path) {
         if (!File.Exists(path)) throw new FileNotFoundException("Could not find settings file", path);
         string json = File.ReadAllText(path);
@@ -17,7 +17,7 @@ public class ConversionSettings {
     }
 
     public static ConversionSettings FromJson(string json) {
-        ConversionSettings? settings = JsonSerializer.Deserialize<ConversionSettings>(json);
+        ConversionSettings? settings = JsonSerializer.Deserialize<ConversionSettings>(json, jsonOpt);
         if (settings is null) throw new NullReferenceException("Could not deserialize settings file");
         return settings;
     }
@@ -26,6 +26,6 @@ public class ConversionSettings {
 
     public void Save(string path) => File.WriteAllText(path, ToJson());
 
-    public string ToJson() => JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true, IncludeFields = true });
+    public string ToJson() => JsonSerializer.Serialize(this, jsonOpt);
 
 }
